466d2a34d6523019f68c3286641ed888441c5263
Brunna Serafina <brunnaserafina@gmail.com>
2023-04-12 11:30:02 -0300

    feat: adding form

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <main class="colums is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline">
     <div class="column is-one-quarter">
       <SidebarMenu />
     </div>
-    <div class="column is-three-quarter"></div>
+    <div class="column is-three-quarter">
+      <FormApp />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import SidebarMenu from "./components/SidebarMenu.vue";
+import FormApp from "./components/FormApp.vue";
 
 export default defineComponent({
   name: "App",
-  components: { SidebarMenu },
+  components: { SidebarMenu, FormApp },
 });
 </script>
 
