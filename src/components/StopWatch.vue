3a5f6b1082b84105f3ac9300f09f2437fc1da730
Brunna Serafina <brunnaserafina@gmail.com>
2023-04-14 21:56:16 -0300

    feat: disabling play and pause buttons

--- src/components/StopWatch.vue
+++ src/components/StopWatch.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <TimerComp :timeInSeconds="timeInSeconds" />
-    <button class="button" @click="startCounting">
+    <button class="button" @click="startCounting" :disabled="timerRunning">
       <span class="icon">
         <i class="fas fa-play"></i>
       </span>
       <span>play</span>
     </button>
-    <button class="button" @click="finishCounting">
+    <button class="button" @click="finishCounting" :disabled="!timerRunning">
       <span class="icon">
         <i class="fas fa-stop"></i>
       </span>
@@ -27,15 +27,18 @@ export default defineComponent({
     return {
       timeInSeconds: 0,
       timer: 0,
+      timerRunning: false,
     };
   },
   methods: {
     startCounting() {
+      this.timerRunning = true;
       this.timer = setInterval(() => {
         this.timeInSeconds += 1;
       }, 1000);
     },
     finishCounting() {
+      this.timerRunning = false;
       clearInterval(this.timer);
     },
   },
